f9054630d8a5d40575d12e4ca8429d2ff39d85ac
Dave Laing <dave.laing.80@gmail.com>
2017-04-12 22:30:35 +1000

    More tweaks to try to get the front end going

--- frontend/default.nix
+++ frontend/default.nix
@@ -2,11 +2,14 @@
 , pkgs ? reflex-platform.nixpkgs.pkgs
 , common ? import ../common }:
 let 
+  doJailbreak = reflex-platform.lib.doJailbreak;
   cabal2nixResult = reflex-platform.cabal2nixResult;
   ghcjs = reflex-platform.ghcjs;
   ghcjsPackages = reflex-platform.ghcjsPackages.override {
     overrides = self: super: {
-      servant-reflex = cabal2nixResult ../servant-reflex;
+      servant-reflex = doJailbreak (cabal2nixResult ../servant-reflex);
+      dependent-sum = super.dependent-sum_0_4;
+      dependent-sum-template = doJailbreak super.dependent-sum-template;
     };
   };
   drv = ghcjsPackages.callPackage ./frontend.nix {
